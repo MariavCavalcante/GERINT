590b5ea0f71de61c637387855e7ba19b27c9c478
MariavCavalcante <cheleenfermeira@hotmail.com>
2026-08-11 17:07:43 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+window.CONFIG = {
+
+    URL_BASE_ATUALIZADA:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRak5nVb3L35cJeEi_CsL1qs60Y8OfCaf8u3jG7vBmb0VbjJCCR5QhA6f4lBPiLX-SnHJP0mwRMp7A9/pub?output=csv",
+
+    URL_DISTANCIAS:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRak5nVb3L35cJeEi_CsL1qs60Y8OfCaf8u3jG7vBmb0VbjJCCR5QhA6f4lBPiLX-SnHJP0mwRMp7A9/pub?gid=53459734&single=true&output=csv",
+
+    CACHE_KEYS: {
+        base: "sesgo_regulacao_base",
+        distancia: "sesgo_regulacao_distancia",
+        sync: "sesgo_regulacao_sync",
+        tema: "sesgo_regulacao_tema"
+    },
+
+    HOSPITAL_ALIASES: {
+        "HUGOL":
+            "HOSPITAL ESTADUAL DE URGENCIAS GOVERNADOR OTAVIO LAGE DE SIQUEIRA",
+
+        "HUGOL GOIANIA":
+            "HOSPITAL ESTADUAL DE URGENCIAS GOVERNADOR OTAVIO LAGE DE SIQUEIRA",
+
+        "HOSPITAL HUGOL":
+            "HOSPITAL ESTADUAL DE URGENCIAS GOVERNADOR OTAVIO LAGE DE SIQUEIRA",
+
+        "HUGO":
+            "HOSPITAL ESTADUAL DE URGENCIAS DE GOIAS DR VALDEMIRO CRUZ",
+
+        "HGG":
+            "HOSPITAL ESTADUAL GERAL DR ALBERTO RASSI",
+
+        "HEAPA":
+            "HOSPITAL ESTADUAL DE APARECIDA DE GOIANIA CAIRO LOUZADA",
+
+        "HEANA":
+            "HOSPITAL ESTADUAL DE ANAPOLIS DR HENRIQUE SANTILLO",
+
+        "CRER":
+            "CENTRO ESTADUAL DE REABILITACAO E READAPTACAO DR HENRIQUE SANTILLO"
+    }
+};
